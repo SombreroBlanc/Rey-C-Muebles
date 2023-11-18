93f401212111be740408b069f0a9a4b32a701de6
SombreroBlanco <151406961+SombreroBlanc@users.noreply.github.com>
2023-11-18 17:44:45 -0500

    ReyCMuebles

--- ReyCMuebles/js/main.js
+++
@@ -1,5 +0,0 @@
-// Simula la función de ver detalle del producto
-function verDetalle(productoId) {
-    // Puedes redirigir a una página de detalle o mostrar un modal, según tu preferencia
-    alert("Mostrar detalle del producto " + productoId);
-}
