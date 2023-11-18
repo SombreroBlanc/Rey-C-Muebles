63f384047e6d8427b3e1b6bc09e45c229524a060
SombreroBlanc <151406961+SombreroBlanc@users.noreply.github.com>
2023-11-18 17:06:35 -0500

    Add files via upload

---
+++ ReyCMuebles/js/main.js
@@ -0,0 +1,5 @@
+// Simula la función de ver detalle del producto
+function verDetalle(productoId) {
+    // Puedes redirigir a una página de detalle o mostrar un modal, según tu preferencia
+    alert("Mostrar detalle del producto " + productoId);
+}
